8e963b50876973a77ffba506b3c29fc0c51f4d36
Nidal <nzoumita@myges.fr>
2023-03-28 20:41:25 +0200

    :wrench: Add mongoDB connection. Update .gitignore

--- config/configuration.ts
+++ config/configuration.ts
@@ -4,5 +4,6 @@ export default () => ({
     engine: process.env.DATABASE_ENGINE,
     host: process.env.DATABASE_HOST,
     port: parseInt(process.env.DATABASE_PORT, 10) || 27017,
+    uri: 'mongodb://127.0.0.1:27017/stats_smogon',
   },
 });
